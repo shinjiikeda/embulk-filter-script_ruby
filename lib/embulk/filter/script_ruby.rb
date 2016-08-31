3714198fcc7f215403b041c74fb09f4f533d2771
shinjiikeda <gm.ikeda@gmail.com>
2016-08-31 15:49:07 +0900

    update

--- lib/embulk/filter/script_ruby.rb
+++ lib/embulk/filter/script_ruby.rb
@@ -1,38 +1,37 @@
 module Embulk
   module Filter
-
     class ScriptRuby < FilterPlugin
-      Plugin.register_filter("script_ruby", self)
+      Plugin.register_filter('script_ruby', self)
 
-      def self.transaction(config, in_schema, &control)
+      def self.transaction(config, _in_schema)
         # configuration code:
         task = {
-            'script'  => config.param('script', :string),
-            'class'  => config.param('class', :string),
-            'columns' => config.param('columns', :array, default: [])
+          'script' => config.param('script', :string),
+          'class' => config.param('class', :string),
+          'columns' => config.param('columns', :array, default: [])
         }
-        
+
         c = 0
-        out_columns = task['columns'].map do | e | 
+        out_columns = task['columns'].map do |e|
           col = Column.new(c, e['name'], e['type'].to_sym)
-          c+=1
-	      col
+          c += 1
+          col
         end
-        
+
         yield(task, out_columns)
       end
 
       def init
         # initialization code:
         @script = task['script']
-       
+
         @out_map = {}
-        out_schema.each do | e |
+        out_schema.each do |e|
           @out_map[e['name']] = true
         end
-        
+
         require @script
-        @filter_class = Object.const_get(task['class']).new()
+        @filter_class = Object.const_get(task['class']).new
       end
 
       def close
@@ -45,12 +44,12 @@ module Embulk
             h = Hash[in_schema.names.zip(record)]
             result = @filter_class.filter(h)
             out_record = []
-            out_schema.sort_by{|e| e['index']}.each do | e |
-              out_record << (result.has_key?(e['name']) ? result[e['name']] : nil)
+            out_schema.sort_by { |e| e['index'] }.each do |e|
+              out_record << (result.key?(e['name']) ? result[e['name']] : nil)
             end
-            page_builder.add(out_record) if out_record.size > 0
+            page_builder.add(out_record) unless out_record.empty?
           rescue => e
-            raise e.to_s + " backtrace: " + e.backtrace.to_s 
+            raise e.to_s + ' backtrace: ' + e.backtrace.to_s
           end
         end
       end
@@ -59,7 +58,5 @@ module Embulk
         page_builder.finish
       end
     end
-    
-    
   end
 end
